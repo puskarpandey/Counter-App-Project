0b8712adfb9b7b8600c10d164a6e550c4ecede4d
“Puskar <puskarpandey96@gmail.com>
2023-07-25 17:13:35 +0530

    Conter-app-project

---
+++ index.js
@@ -0,0 +1,19 @@
+const countValue=document.querySelector('#counter');
+
+function increment(){
+    // get the vlue from UI
+    let value = parseInt(countValue.innerText);
+    //Update the value
+    value = value + 1;
+    //set the value onto UI
+    countValue.innerText = value;
+};
+
+function decrement(){
+        // get the vlue from UI
+        let value = parseInt(countValue.innerText);
+        //Update the value
+        value = value - 1;
+        //set the value onto UI
+        countValue.innerText = value;
+};
